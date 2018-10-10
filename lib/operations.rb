0eea01efa6bfbb17af612d231d5927fe15b2fad9
Emily Asch <e.asch@me.com>
2018-10-10 01:52:43 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -23,12 +23,9 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-  speed > 60 ? true : false
+  speed <= 40 || speed >= 60 ? true : false
 end
 
-def not_safe?(speed)
-  speed < 40 ? true : false
-end
 
 
 	
